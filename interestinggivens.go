d45169c72f6a7da340d58d8f5370f403ec712596
Matt <matt.corby@gmail.com>
2018-01-09 15:39:21 +0000

    Update interestinggivens.go

--- interestinggivens.go
+++ interestinggivens.go
@@ -1,5 +1,5 @@
 package gogiven
-//Interesting givens contains a map which can hold the values used to set up the test.
+//InterestingGivens contains a map which can hold the values used to set up the test.
 // You can add actual vs. expected output for example.
 // InterestingGivens is created by the test framework and passed into the When() and Then()
 // functions.
