536cb0bd6752bb179b69f4442ab26c717934d61f
Matt <matt.corby@gmail.com>
2018-01-09 15:34:39 +0000

    Update testcontext.go

--- testcontext.go
+++ testcontext.go
@@ -3,33 +3,36 @@ package gogiven
 import (
 	"fmt"
 )
-
+// TestMetaData holds some information about the test, it's id, whether it failed or not
+// and the output it sent to t.Errorf or t.Error etc.
 type TestMetaData struct {
 	TestId     string
 	Failed     bool
 	TestOutput string
 }
-
+//Creates a new TestMetaData object. Used internally.
 func NewTestMetaData(testName string) *TestMetaData {
 	testContext := new(TestMetaData)
 	testContext.TestId = testName
 	testContext.Failed = false
 	return testContext
 }
+//Marks this test as failed and sets the test output to the formatted string. 
 func (t *TestMetaData) Logf(format string, args ...interface{}) {
 	t.TestOutput = fmt.Sprintf(format, args...)
 	t.Failed = true
 }
-
+//Marks this test as failed and sets the test output to the formatted string. 
 func (t *TestMetaData) Errorf(format string, args ...interface{}) {
 	t.TestOutput = fmt.Sprintf(format, args...)
 	t.Failed = true
 }
-
+//Marks this test as failed. 
 func (t *TestMetaData) FailNow() {
 	t.Failed = true
 }
-
+//This implementation of Helper() does nothing. It's just in case some package that consumes t 
+// calls it.
 func (t *TestMetaData) Helper() {
 	// do nothing
 }
