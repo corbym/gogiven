9f2ab0a46e1f5042cdff90f194bc9e7c2cf2742e
Matt <matthew.corby.eaglen@itv.com>
2018-01-09 15:19:39 +0000

    Added godoc to capturedio.go

--- capturedio.go
+++ capturedio.go
@@ -1,5 +1,9 @@
 package gogiven
-
+//CapturedIO contains the captured inputs and outputs for the test.
+// These could be the interactions between the system and the stubbed endpoints,
+// or any value that the system under test produces.
+// Underlying the CapturedIO is a map[string]interface{}.
+// CapturedIO objects are provided to the test by the When() and Then() functions.
 type CapturedIO struct {
 	CapturedIO map[string]interface{}
 }
