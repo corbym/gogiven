1bcb590d64dad4341e7dc29344def5e67727a3a4
Matt <matt.corby@gmail.com>
2018-01-09 15:29:23 +0000

    Update interestinggivens.go

--- interestinggivens.go
+++ interestinggivens.go
@@ -1,5 +1,8 @@
 package gogiven
-
+//Interesting givens contains a map which can hold the values used to set up the test.
+// You can add actual vs. expected output for example.
+// InterestingGivens is created by the test framework and passed into the When() and Then()
+// functions.
 type InterestingGivens struct {
 	Givens map[string]interface{} ``
 }
