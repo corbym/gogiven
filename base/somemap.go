2f3d55fd5cfbaef512fb3dd6d2f1309b88e894b4
Matt Corby <matthew.corby.eaglen@itv.com>
2018-01-13 22:40:33 +0000

    go fmt

--- base/somemap.go
+++ base/somemap.go
@@ -1,3 +1,4 @@
 package base
+
 // SomeMap is a typed map that contains a reference to test data held in a Some struct.
 type SomeMap map[string]*Some
--- generator/generator.go
+++ generator/generator.go
@@ -2,7 +2,7 @@ package generator
 
 // GoGivensOutputGenerator is an interface that can be implemented by anything that can generate file content to be output
 // after a test has completed.
-type GoGivensOutputGenerator interface{
+type GoGivensOutputGenerator interface {
 	Generate(data *PageData) (output string)
 	FileExtension() string
 }
